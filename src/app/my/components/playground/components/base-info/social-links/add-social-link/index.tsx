a6037f7893474bc41fd836d962c252fdfa8d5e78
SIYUAN YANG <sephenry@hotmail.com>
2025-06-02 00:12:29 +0800

    :bulb: 调整 chunkName 的格式

--- src/app/my/components/playground/components/base-info/social-links/add-social-link/index.tsx
+++ src/app/my/components/playground/components/base-info/social-links/add-social-link/index.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import Loading from '@/app/components/loading';
 
 const AddSocialLink = dynamic(
-  () => import(/* webpackChunkName: AddSocialLink */ './component'),
+  () => import(/* webpackChunkName: "AddSocialLink" */ './component'),
   {
     ssr: false,
     loading: () => <Loading />,
